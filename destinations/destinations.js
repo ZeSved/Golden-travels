cc707f68a6cfc2a09c3938bd9928ac42c1bb5954
Timothy Korell <timothy.korell@gmail.com>
2024-09-05 14:23:37 +0200

    added more images, fixed bug

--- destinations/destinations.js
+++ destinations/destinations.js
@@ -1,2 +1,4 @@
-const map = document.getElementById('google-map')
-map.src = `https://www.google.com/maps/embed/v1/place?key=AIzaSyBJJNyhCEg8sxLsO6YMZ-GMFTT-c5-Cz_Q&q=Grand+Hotel,Stockholm+Sweden`
+const cityMap = document.getElementById('city-map')
+cityMap.src = `https://www.google.com/maps/embed/v1/place?key=AIzaSyBJJNyhCEg8sxLsO6YMZ-GMFTT-c5-Cz_Q&q=Stockholm+Sweden`
+const hotelMap = document.getElementById('hotel-map')
+hotelMap.src = `https://www.google.com/maps/embed/v1/place?key=AIzaSyBJJNyhCEg8sxLsO6YMZ-GMFTT-c5-Cz_Q&q=Grand+Hotel,Stockholm+Sweden`
--- main.js
+++ main.js
@@ -8,17 +8,24 @@ const images = [
 	'images/20-Best-Things-To-Do-In-Brazil-Christo.jpg',
 	'images/africa-best-places-to-visit-experience-victoria-falls-zimbabwe-zambia.jpg',
 	'images/asia-best-places-to-visit-bagan-myanmar.jpg',
-	'images/16-colosseum-getty.jpg',
 	'images/Cape-Town-aerial-shot-e1477915766935.jpg',
 	'images/Chichen-750x400.webp',
-	'images/nedladdning.jpg',
+	'images/eiffel-tower-outro-stock.jpg',
+	'images/most-visited-tourist-attraction-world--the-met.png',
+	'images/most-visited-tourist-attraction-world-dinsey-california-adventure.png',
+	'images/most-visited-tourist-attraction-world-islands-of-adventure.png',
+	'images/most-visited-tourist-attraction-world-lake-mead.png',
+	'images/most-visited-tourist-attraction-world-smoky-mountains.png',
+	'images/most-visited-tourist-attraction-world-unviersal-studios-hollywood.png',
+	'images/most-visited-tourist-attraction-world-unviersal-sydney-opera-house.png',
+	'images/pexels-photo-753626.jpeg',
 	'images/shutterstock-580489630.webp',
 	'images/test.webp',
 	'images/the-great-wall-2190047_960_720-e1517999791900.jpg',
+	'images/victoria-peak-most-visited-tourist-attraction-world.png',
 ]
 
 document.addEventListener('scroll', () => {
-	// const FIRST_ANIMATION_CONDITION = window.scrollY >= 200
 	const FIRST_ANIMATION_CONDITION = window.scrollY >= window.outerHeight / 5
 	const SECOND_ANIMATION_CONDITION = window.scrollY >= (window.outerHeight / 5) * 4
 
@@ -31,9 +38,9 @@ photoElements.forEach((photo) => {
 })
 
 function changeImages(elm) {
-	const path = images[Math.floor(Math.random() * (9 - 0 + 1) + 0)]
+	const path = images[Math.floor(Math.random() * (images.length - 1 - 0 + 1) + 0)]
 
-	if (elm.src === path) {
+	if (elm.src.includes(path)) {
 		changeImages(elm)
 	} else {
 		elm.src = path
@@ -41,9 +48,6 @@ function changeImages(elm) {
 }
 
 function startAnimations(section, condition) {
-	// console.log(window.innerHeight)
-	// console.log(window.outerHeight)
-
 	if (section.classList.contains('slide-in')) {
 		if (condition) return
 
