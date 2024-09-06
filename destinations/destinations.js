d8c73301f2810b157af8872fd4f4c4553ba73ca2
ZeSved <timothy.korell@gmail.com>
2024-09-06 21:38:53 +0200

    improved js massively

--- destinations/destinations.js
+++ destinations/destinations.js
@@ -6,23 +6,49 @@ const hotelInput = document.getElementById('hotel-input')
 const next = document.getElementById('next')
 const previous = document.getElementById('previous')
 
-const bar1 = document.getElementById('bar1')
-const bar2 = document.getElementById('bar2')
-const step1 = document.getElementById('step1')
-const step2 = document.getElementById('step2')
-const step3 = document.getElementById('step3')
+const bar_1 = document.getElementById('bar-1')
+const bar_2 = document.getElementById('bar-2')
+const step_1 = document.getElementById('step-1')
+const step_2 = document.getElementById('step-2')
+const step_3 = document.getElementById('step-3')
 
 const tripLocation = document.getElementById('location')
 const dates = document.getElementById('dates')
 const confirmDetails = document.getElementById('confirm')
 
-const currentlyActive = ['step1']
+const currentlyActive = ['step_1']
+const classToggles = {
+	progressBar: [
+		[bar_1, 'active', 'unactive'],
+		[step_2, 'active', 'unactive'],
+		[bar_2, 'active', 'unactive'],
+		[step_3, 'active', 'unactive'],
+	],
+	page: [
+		[tripLocation, 'hidden', 'current'],
+		[dates, 'current', 'hidden'],
+		[dates, 'hidden', 'current'],
+		[confirmDetails, 'current', 'hidden'],
+	],
+	progressBarReverse: [
+		[bar_1, 'unactive', 'active'],
+		[step_2, 'unactive', 'active'],
+		[bar_2, 'unactive', 'active'],
+		[step_3, 'unactive', 'active'],
+	],
+	pageReverse: [
+		[tripLocation, 'current', 'hidden'],
+		[dates, 'hidden', 'current'],
+		[dates, 'current', 'hidden'],
+		[confirmDetails, 'hidden', 'current'],
+	],
+}
 
 const FORWARD_ANIMATION_DELAY = 600
-const BACK_ANIMATION_DELAY = FORWARD_ANIMATION_DELAY
+const BACK_ANIMATION_DELAY = 350
 
-next.addEventListener('click', () => handleTransition(true))
-previous.addEventListener('click', () => handleTransition(false))
+next.addEventListener('click', () => handleAnimations(true))
+previous.addEventListener('click', () => handleAnimations(false))
 
 cityInput.addEventListener('change', (e) => updateMaps(e, 'city'))
 hotelInput.addEventListener('change', (e) => updateMaps(e, 'hotel'))
@@ -40,50 +66,48 @@ function updateMaps(e, map) {
 		.replaceAll(' ', '+')}`
 }
 
-function handleTransition(goToNext) {
+function handleAnimations(goToNext) {
+	const activeLen = currentlyActive.length
+	const { progressBar, page, progressBarReverse, pageReverse } = classToggles
+
 	if (goToNext) {
-		if (currentlyActive.length === 1) {
-			toggleClassnames(bar1, 'active', 'unactive')
-			toggleClassnames(step2, 'active', 'unactive')
-			currentlyActive.push('bar1', 'step2')
-			setTimeout(() => conicGradient(true, step2), FORWARD_ANIMATION_DELAY)
+		toggleClassnames(
+			progressBar[activeLen - 1],
+			progressBar[activeLen],
+			page[activeLen - 1],
+			page[activeLen]
+		)
 
-			toggleClassnames(tripLocation, 'hidden', 'current')
-			toggleClassnames(dates, 'current', 'hidden')
-		} else if (currentlyActive.length === 3) {
-			toggleClassnames(bar2, 'active', 'unactive')
-			toggleClassnames(step3, 'active', 'unactive')
-			currentlyActive.push('bar2', 'step3')
-			setTimeout(() => conicGradient(true, step3), FORWARD_ANIMATION_DELAY)
-
-			toggleClassnames(dates, 'hidden', 'current')
-			toggleClassnames(confirmDetails, 'current', 'hidden')
+		if (activeLen === 1) {
+			currentlyActive.push('bar_1', 'step_2')
+		} else {
+			currentlyActive.push('bar_2', 'step_3')
 		}
+
+		setTimeout(
+			() => conicGradient(true, activeLen === 1 ? step_2 : step_3),
+			FORWARD_ANIMATION_DELAY
+		)
 	} else {
-		if (currentlyActive.length === 5) {
-			conicGradient(false, step3)
-			setTimeout(() => toggleClassnames(bar2, 'unactive', 'active'), BACK_ANIMATION_DELAY)
-			setTimeout(() => toggleClassnames(step3, 'unactive', 'active'), BACK_ANIMATION_DELAY)
+		if (activeLen === 1) return
 
-			toggleClassnames(confirmDetails, 'hidden', 'current')
-			toggleClassnames(dates, 'current', 'hidden')
-		} else if (currentlyActive.length === 3) {
-			conicGradient(false, step2)
-			setTimeout(() => toggleClassnames(bar1, 'unactive', 'active'), BACK_ANIMATION_DELAY)
-			setTimeout(() => toggleClassnames(step2, 'unactive', 'active'), BACK_ANIMATION_DELAY)
-
-			toggleClassnames(dates, 'hidden', 'current')
-			toggleClassnames(tripLocation, 'current', 'hidden')
-		} else return
+		conicGradient(false, progressBarReverse[activeLen - 2][0])
+		setTimeout(
+			() => toggleClassnames(progressBarReverse[activeLen - 3], progressBarReverse[activeLen - 2]),
+			BACK_ANIMATION_DELAY
+		)
+		toggleClassnames(pageReverse[activeLen - 3], pageReverse[activeLen - 2])
 
 		currentlyActive.pop()
 		currentlyActive.pop()
 	}
 }
 
-function toggleClassnames(item, addName, removeName) {
-	item.classList.add(addName)
-	item.classList.remove(removeName)
+function toggleClassnames(...classArrays) {
+	Array.from(classArrays).forEach((item) => {
+		item[0].classList.add(item[1])
+		item[0].classList.remove(item[2])
+	})
 }
 
 function conicGradient(goToNext, elm) {
