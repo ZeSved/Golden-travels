f6e29a742979e0e16fafef791f9461ffdea5c3c8
ZeSved <timothy.korell@gmail.com>
2024-09-10 21:08:48 +0200

    progress on calendars

--- destinations/datePage.js
+++ destinations/datePage.js
@@ -1,29 +1,85 @@
 const dateGridFrom = document.getElementById('date-grid-from')
 const dateGridTo = document.getElementById('date-grid-to')
-const yearDisplay = document.querySelector('.year')
-const monthDisplay = document.querySelector('.month')
+const firstSection = document.getElementById('from')
+const secondSection = document.getElementById('to')
+
+const firstSectChildren = {
+	previous: firstSection.querySelector('.previous'),
+	next: firstSection.querySelector('.next'),
+	year: firstSection.querySelector('.year'),
+	month: firstSection.querySelector('.month'),
+	difference: 0,
+}
+
+const secondSectChildren = {
+	previous: secondSection.querySelector('.previous'),
+	next: secondSection.querySelector('.next'),
+	year: secondSection.querySelector('.year'),
+	month: secondSection.querySelector('.month'),
+	difference: 0,
+}
+
+const eventListeners = [
+	[firstSectChildren, firstSectChildren.next, 1, dateGridFrom],
+	[firstSectChildren, firstSectChildren.previous, -1, dateGridFrom],
+	[secondSectChildren, secondSectChildren.next, 1, dateGridTo],
+	[secondSectChildren, secondSectChildren.previous, -1, dateGridTo],
+]
+
+const onInitalLoad = [firstSectChildren, secondSectChildren]
 
 const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
-const YEAR = new Date().getFullYear()
-const MONTH = new Date().getMonth()
-// const MONTH = 0
-const DAY = new Date().getDay()
+const currentYear = new Date().getFullYear()
+const currentMonth = new Date().getMonth()
+const currentDay = new Date().getDay()
 
-createDateGrid(dateGridFrom)
-createDateGrid(dateGridTo)
+onInitalLoad.forEach((item) => {
+	item.year.textContent = currentYear
+	item.month.textContent = capitalizeFirst(
+		new Date(currentYear, currentMonth, currentDay).toLocaleString('default', {
+			month: 'long',
+		})
+	)
+})
 
-monthDisplay.textContent = new Date(YEAR, MONTH, DAY).toLocaleString('default', { month: 'long' })
-yearDisplay.textContent = YEAR
+eventListeners.forEach((item) => {
+	item[1].addEventListener('click', () => changeMonth(item[0], item[2], item[3]))
+})
 
-function createDateGrid(elm) {
+function changeMonth(object, amountChanged, elm) {
+	const obj = { ...object }
+	const { year, month } = obj
+
+	object.difference += amountChanged
+	year.textContent = currentYear
+	month.textContent = capitalizeFirst(
+		new Date(currentYear, currentMonth + object.difference, currentDay).toLocaleString('default', {
+			month: 'long',
+		})
+	)
+
+	createDateGrid(elm, currentMonth + object.difference)
+}
+
+createDateGrid(dateGridFrom, currentMonth)
+createDateGrid(dateGridTo, currentMonth)
+
+function createDateGrid(elm, month) {
 	const daysShown = []
+	const lastDayOfMonth = new Date(currentYear, month + 1, 0).getDate()
+	const currentMonthWeekdayStart = new Date(currentYear, month, 1).toString().substring(0, 3)
+	const lastDayPreviousMonth = new Date(currentYear, month, 0).getDate()
 
-	for (let dayNumber = 1; dayNumber <= new Date(YEAR, MONTH + 1, 0).getDate(); dayNumber++) {
+	while (elm.firstChild) {
+		elm.removeChild(elm.firstChild)
+	}
+
+	for (let dayNumber = 1; dayNumber <= lastDayOfMonth; dayNumber++) {
 		const paragraph = document.createElement('p')
 		paragraph.appendChild(document.createTextNode(dayNumber))
 
-		if (dayNumber === new Date().getDate()) {
+		if (dayNumber === new Date().getDate() && month === currentMonth) {
 			paragraph.className = 'today'
 		}
 
@@ -31,12 +87,8 @@ function createDateGrid(elm) {
 	}
 
 	for (let day = 0; day <= days.length - 1; day++) {
-		if (new Date(YEAR, MONTH, 1).toString().substring(0, 3) === days[day]) {
-			for (
-				let i = new Date(YEAR, MONTH, 0).getDate();
-				i >= new Date(YEAR, MONTH, 0).getDate() - day + 1;
-				i--
-			) {
+		if (currentMonthWeekdayStart === days[day]) {
+			for (let i = lastDayPreviousMonth; i >= lastDayPreviousMonth - day + 1; i--) {
 				const paragraph = document.createElement('p')
 				paragraph.appendChild(document.createTextNode(i))
 				paragraph.className = 'otherMonth'
@@ -47,7 +99,6 @@ function createDateGrid(elm) {
 
 	let i = 1
 	while (daysShown.length < 42) {
-		console.log(daysShown.length)
 		const paragraph = document.createElement('p')
 		paragraph.appendChild(document.createTextNode(i))
 		paragraph.className = 'otherMonth'
@@ -58,4 +109,11 @@ function createDateGrid(elm) {
 	daysShown.forEach((day) => {
 		elm.appendChild(day)
 	})
+}
+
+function capitalizeFirst(str) {
+	const firstLetter = str[0].toUpperCase()
+	const remainingString = str.slice(1)
+
+	return firstLetter + remainingString
 }
